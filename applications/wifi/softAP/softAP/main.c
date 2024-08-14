eb5e68eb08c3946195c62ad4316ff7158255e977
Dang Cong Tao <dangcongtao1999tn@gmail.com>
2024-08-14 09:06:37 +0700

    AP able to handle data come

--- applications/wifi/softAP/softAP/main.c
+++ applications/wifi/softAP/softAP/main.c
@@ -21,10 +21,132 @@
 #include <lwip/inet.h>
 #include <blog.h>
 
+#include "FreeRTOS.h"
+#include "portmacro.h"
+#include "event_groups.h"
+#include "lwip/err.h"
+#include "string.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/api.h"
+#include "web_server.h"
+
 #define AP_SSID "taodc"
 #define AP_PWD "66668888"
 
 #define TAG "softAP"
+#define CARRIAGE_RETURN 13
+#define SERVER_PORT 80
+
+const static char http_html_hdr[] =
+    "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
+const static char http_index_hml[] = "<!DOCTYPE html>"
+                                     "<html>\n"
+                                     "<head>\n"
+                                     "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+                                     "  <style type=\"text/css\">\n"
+                                     "    html, body, iframe { margin: 0; padding: 0; height: 100%; }\n"
+                                     "    iframe { display: block; width: 100%; border: none; }\n"
+                                     "  </style>\n"
+                                     "<title>HELLO Ai-WB2 module</title>\n"
+                                     "</head>\n"
+                                     "<body>\n"
+                                     "<h1>Hello World, from Ai-WB2 module</h1>\n"
+                                     "</body>\n"
+                                     "</html>\n";
+
+
+static void get_body_msg(char *msg)
+{
+    char *token = NULL;
+    token = strtok(msg, "\n");
+    while (token[0] != CARRIAGE_RETURN)
+    {
+        token = strtok(NULL, "\n");
+    }
+    token = strtok(NULL, "\n");
+    if (token == NULL)
+    {
+        blog_error("no body");
+        return;
+    }
+    blog_warn("token [%s] ", token);
+    if (strstr(token, "ssid") != NULL)
+    {
+        blog_info("will connect wifi ");
+    }
+}
+
+static void web_http_server(struct netconn *conn)
+{
+    struct netbuf *inputbuf;
+    char *buf;
+    u16_t buflen;
+    err_t err;
+
+    err = netconn_recv(conn, &inputbuf);
+    if (err == ERR_OK)
+    {
+        netbuf_data(inputbuf, (void **)&buf, &buflen);
+        blog_warn("the received data:\n[%s]\n", buf);
+        get_body_msg(buf);
+
+        /* Judge if this is an HTTP GET command */
+        if (buflen >= 5 && buf[0] == 'G' && buf[1] == 'E' && buf[2] == 'T' && buf[3] == ' ' && buf[4] == '/')
+        {
+            netconn_write(conn, http_html_hdr, sizeof(http_html_hdr) - 1, NETCONN_NOCOPY);
+
+            if (buf[5] == 'h')
+            {
+                netconn_write(conn, http_index_hml, sizeof(http_index_hml) - 1, NETCONN_NOCOPY);
+            }
+            else if (buf[5] == 'l')
+            {
+                netconn_write(conn, http_index_hml, sizeof(http_index_hml) - 1, NETCONN_NOCOPY);
+            }
+            else
+            {
+                netconn_write(conn, http_index_hml, sizeof(http_index_hml) - 1, NETCONN_NOCOPY);
+            }
+        }
+    }
+    netconn_close(conn);
+    netbuf_delete(inputbuf);
+}
+
+void http_server_start(void *pvParameters)
+{
+    struct netconn *conn, *newconn;
+    err_t err;
+    conn = netconn_new(NETCONN_TCP);
+    err = netconn_bind(conn, NULL, SERVER_PORT);
+    if (err != ERR_OK)
+    {
+        blog_error("fail to bind ");
+    }
+    else
+    {
+        blog_info("bind success ");
+    }
+    netconn_listen(conn);
+    while (1)
+    {
+        err = netconn_accept(conn, &newconn);
+        if (err == ERR_OK)
+        {
+            web_http_server(newconn);
+            netconn_delete(newconn);
+        }
+        else
+        {
+            netconn_close(conn);
+            netconn_delete(conn);
+            break;
+        }
+    }
+}
+
+
 
 static wifi_conf_t ap_conf = {
     .country_code = "CN",
@@ -109,16 +231,18 @@ static void event_cb_wifi_event(input_event_t* event, void* private_data)
             wifi_ap_start();
             break;
         case CODE_WIFI_ON_AP_STARTED:
+        {
             blog_info("<<<<<<<<< startt soft ap OK<<<<<<<<<<<");
-
-            break;
+            /* start http server */
+            xTaskCreate(http_server_start, (char *)"http server", 1024 * 4, NULL, 15, NULL);
+        } break;
         case CODE_WIFI_ON_AP_STOPPED:
             break;
         case CODE_WIFI_ON_AP_STA_ADD:
+        {
             blog_info("<<<<<<<<< station connent ap <<<<<<<<<<<");
             /* device connect wifi */
-
-            break;
+        } break;
         case CODE_WIFI_ON_AP_STA_DEL:
             blog_info("<<<<<<<<< station disconnet ap <<<<<<<<<<<");
 
